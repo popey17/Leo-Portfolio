ce079ddc8e6e44ba3223b6a00e272b04e5bbab21
popey17 <contact.popey17@gmail.com>
2023-03-06 19:26:03 +0630

    fix lintererror and styles

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav-content');
+// const closeBtn = document.querySelector('.close-btn')
 
 menuBtn.onclick = function show() {
   nav.classList.toggle('show');
+  menuBtn.classList.toggle('fa-bars');
+  menuBtn.classList.toggle('fa-xmark');
 };
