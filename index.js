b85d97e2961c18b82412abdbf3a22a9cddce0d91
popey17 <contact.popey17@gmail.com>
2023-03-06 19:37:49 +0630

    fix styles and errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav-content');
-// const closeBtn = document.querySelector('.close-btn')
+const closeBtn = document.querySelector('.close-btn')
 
 menuBtn.onclick = function show() {
-  nav.classList.toggle('show');
-  menuBtn.classList.toggle('fa-bars');
-  menuBtn.classList.toggle('fa-xmark');
+  nav.classList.add('show');
 };
+
+closeBtn.onclick = function close() {
+  nav.classList.remove('show');
+}
