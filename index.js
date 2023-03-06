1a3cbd95fc5151cfd130bd3e57f7cc405cbc54ee
popey17 <contact.popey17@gmail.com>
2023-03-06 19:47:54 +0630

    fix linter errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav-content');
-const closeBtn = document.querySelector('.close-btn')
+const closeBtn = document.querySelector('.close-btn');
 
 menuBtn.onclick = function show() {
   nav.classList.add('show');
@@ -8,4 +8,4 @@ menuBtn.onclick = function show() {
 
 closeBtn.onclick = function close() {
   nav.classList.remove('show');
-}
+};
